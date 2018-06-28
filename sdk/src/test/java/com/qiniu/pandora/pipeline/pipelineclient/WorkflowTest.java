ad144fe434d4c62974c4d6732e04853e320b2fed
litianqi <litianqi@qiniu.com>
2018-06-28 10:01:54 +0800

    import org.junit.FixMethodOrder

--- sdk/src/test/java/com/qiniu/pandora/pipeline/pipelineclient/WorkflowTest.java
+++ sdk/src/test/java/com/qiniu/pandora/pipeline/pipelineclient/WorkflowTest.java
@@ -3,17 +3,19 @@ package com.qiniu.pandora.pipeline.pipelineclient;
 import com.qiniu.pandora.common.PandoraClient;
 import com.qiniu.pandora.common.PandoraClientImpl;
 import com.qiniu.pandora.common.QiniuException;
-import com.qiniu.pandora.pipeline.common.TestConfig;
+import com.qiniu.pandora.common.TestConfig;
 import com.qiniu.pandora.pipeline.service.PipelineClient;
 import com.qiniu.pandora.pipeline.repo.*;
 import com.qiniu.pandora.util.Auth;
+import org.junit.runners.MethodSorters;
+import org.junit.FixMethodOrder;
 
 import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class WorkflowTest {
     protected PipelineClient pipelineClient;
 
@@ -26,7 +28,7 @@ public class WorkflowTest {
 
 
     @Test
-    public void testcreateWorkflow() throws QiniuException {
+    public void test001createWorkflow() throws QiniuException {
         CreateWorkflowInput createWorkflowInput = new CreateWorkflowInput();
         createWorkflowInput.comment = "javasdktest";
         createWorkflowInput.region = "nb";
@@ -35,17 +37,18 @@ public class WorkflowTest {
     }
 
     @Test
-    public void testupdateWorkflow() throws QiniuException {
+    public void test002updateWorkflow() throws QiniuException {
         UpdateWorkflowInput updateWorkflowInput = new UpdateWorkflowInput();
         updateWorkflowInput.region = "nb";
         updateWorkflowInput.workflowName = "javasdk";
+        updateWorkflowInput.nodes=null;
         pipelineClient.updateWorkflow(updateWorkflowInput);
     }
 
 
 
     @Test
-    public void testlistWorkflows() throws QiniuException {
+    public void test003listWorkflows() throws QiniuException {
         GetWorkflowOutput[] getWorkflowOutputs = pipelineClient.listWorkflows();
         for (GetWorkflowOutput i : getWorkflowOutputs) {
             System.out.println(i.toString());
@@ -53,14 +56,14 @@ public class WorkflowTest {
     }
 
     @Test
-    public void testgetWorkflow() throws QiniuException {
+    public void test004getWorkflow() throws QiniuException {
         String workflowname = "javasdk";
         GetWorkflowOutput getWorkflowOutput = pipelineClient.getWorkflow(workflowname);
         System.out.println(getWorkflowOutput.toString());
     }
 
     @Test
-    public void testgetWorkflowsStatus() throws QiniuException {
+    public void test005getWorkflowsStatus() throws QiniuException {
         String workflowname = "javasdk";
         GetWorkflowStatus getWorkflowStatus = pipelineClient.getWorkflowStatus(workflowname);
         System.out.println(getWorkflowStatus.toString());
@@ -68,13 +71,13 @@ public class WorkflowTest {
 
 
     @Test
-    public void testworkflowExists() throws QiniuException {
+    public void test006workflowExists() throws QiniuException {
         String workflowname = "javasdk";
         System.out.println(pipelineClient.getWorkflowStatus(workflowname));
     }
 
     @Test
-    public void testdeleteWorkflow() throws QiniuException {
+    public void test007deleteWorkflow() throws QiniuException {
         String workflowname = "javasdk";
         pipelineClient.deleteWorkflow(workflowname);
     }
