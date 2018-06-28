708bb348279b3f5245b47fd6206fe712976ba53f
Jemy <jemygraw@gmail.com>
2018-06-28 19:52:47 +0800

    rename analyzer and update to version 2.0.0

--- sdk/src/main/java/com/qiniu/pandora/logdb/repo/Analyzer.java
+++ sdk/src/main/java/com/qiniu/pandora/logdb/repo/Analyzer.java
@@ -3,7 +3,7 @@ package com.qiniu.pandora.logdb.repo;
 /**
  * Created by jemy on 2018/6/25.
  */
-public interface Analyser {
+public interface Analyzer {
     String StandardAnalyzer = "standard";
     String SimpleAnalyzer = "simple";
     String WhitespaceAnalyzer = "whitespace";
